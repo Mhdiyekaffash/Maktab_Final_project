eb7d2d7a3dcceef1aebe544cc67a109ec69930f7
Mahdiye <91253157+Mhdiyekaffash@users.noreply.github.com>
2022-04-17 22:23:46 +0430

    Update settings.py

--- SRC/Messenger__/Messenger_/settings.py
+++ SRC/Messenger__/Messenger_/settings.py
@@ -166,8 +166,8 @@ LOGIN_URL = "login"  # If the user is not logged in, it goes back to the login p
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'kfashmhdyh@gmail.com'
-EMAIL_HOST_PASSWORD = 'gzpxckejwndantsd'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 DBBACKUP_CONNECTORS = {
